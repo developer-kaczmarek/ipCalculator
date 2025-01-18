21f520ec55aabcaa70a35e03066513cba2dda760
Lina Podbolotova <41150309+developer-kaczmarek@users.noreply.github.com>
2025-01-18 22:39:51 +0500

    refactor: Migrate to Jetpack Compose and Kotlin DSL (#6)

---
+++ app/src/main/java/io/github/kaczmarek/ipcalculator/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package io.github.kaczmarek.ipcalculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF0381FE)
+val SecondaryLightColor = Color(0xFF0072DE)
+val SecondaryDarkColor = Color(0xFF3E91FF)
+val ControlActivateColor = Color(0xFF3E91FF)
+
+val BackgroundLightColor = Color(0xFFF7F7F7)
+val BackgroundDarkColor = Color(0xFF000000)
+
+val SurfaceLightColor = Color(0xFFFFFFFF)
+val SurfaceDarkColor = Color(0xFF171717)
