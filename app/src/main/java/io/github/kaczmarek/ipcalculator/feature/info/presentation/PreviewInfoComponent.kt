a5924a61e0b15fedcef5771fffa7255fbc7865de
Lina Podbolotova <developer.kaczmarek@gmail.com>
2025-02-03 23:31:55 +0500

    refactor: minor changes

--- app/src/main/java/io/github/kaczmarek/ipcalculator/feature/info/presentation/PreviewInfoComponent.kt
+++ app/src/main/java/io/github/kaczmarek/ipcalculator/feature/info/presentation/PreviewInfoComponent.kt
@@ -1,6 +1,7 @@
 package io.github.kaczmarek.ipcalculator.feature.info.presentation
 
 class PreviewInfoComponent : InfoComponent {
+
     override fun onOpenGithubPageClick() = Unit
 
     override fun onReadPrivacyPolicyClick() = Unit
